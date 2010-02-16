3929b57ea98b98f21ca5e6a33edd26986cb6a9d8
Joseph Szobody <jszobody@gmail.com>
2010-02-16 08:45:34 -0600

    Updated to show how to talk from listener

--- examplelistener.php
+++ examplelistener.php
@@ -3,8 +3,11 @@ require_once("lib/class.baselistener.php");
 
 class Listener extends BaseListener {
 	function on_message($user, $message) {
-		echo "We got a message from {$user['name']}: $message\n";
+		if($user['name'] != $this->talker->user['name']) $this->talker->send_message("We got a message from {$user['name']}: $message");
+	}
+	
+	function on_join($user) {
+		$this->talker->send_message("Heya {$user['name']}, so nice of you to grace us with your presence");
 	}
 }
-
 ?>
